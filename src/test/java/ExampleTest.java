4d78d40faf767d2d9732413fc906b8216426427e
soundt <soundt@amazon.com>
2020-01-04 20:38:01 -0800

    created a new file Hello.txt

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,10 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ExampleTest {
 
     @Test
-    public void test1() {
+    public void test1() throws IOException {
         Assert.assertEquals(1,1);
+        String str = "Hello";
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Hello.txt"));
+        writer.write(str);
+        writer.close();
     }
 }
